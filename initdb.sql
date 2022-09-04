9fd6726dd20e90c740a9c34ac71ce5a4c1c5d90a
Mário Monteiro <marioammonteiro@gmail.com>
2022-09-04 23:04:56 +0100

    start to refactor src to lib and finished forRootAsync

--- initdb.sql
+++ initdb.sql
@@ -1,13 +1,22 @@
+s-- implicit full access for new signup users
+DEFINE TABLE person SCHEMAFULL PERMISSIONS
+  FOR select FULL,
+  FOR create FULL,
+  FOR update FULL,
+  FOR delete FULL;
+
 DEFINE TABLE user SCHEMAFULL
   PERMISSIONS 
     FOR select, update WHERE id = $auth.id, 
     FOR create, delete NONE;
+
 DEFINE FIELD email ON user TYPE string;
 DEFINE FIELD pass ON user TYPE string;
 DEFINE FIELD settings.* ON user TYPE object;
 DEFINE FIELD settings.marketing ON user TYPE string;
 DEFINE FIELD tags ON user TYPE array;
 DEFINE INDEX idx_email ON user COLUMNS email UNIQUE;
+
 DEFINE SCOPE allusers
   SESSION 14d
   SIGNUP ( CREATE user SET settings.marketing = $marketing, email = $email, pass = crypto::argon2::generate($pass), tags = $tags )
