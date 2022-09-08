26679ebef83f050d5de7bd85502a18a19b1c31a6
Mário Monteiro <marioammonteiro@gmail.com>
2022-09-08 11:52:24 +0100

    repush

--- tutorial-graphql/src/recipes/recipes.module.ts
+++ tutorial-graphql/src/recipes/recipes.module.ts
@@ -1,4 +1,3 @@
-import { SURREALDB_CONFIG_OPTIONS } from '@koakh/nestjs-surrealdb';
 import { SurrealDbModule, SurrealDbService } from '@koakh/nestjs-surrealdb';
 import { Module } from '@nestjs/common';
 import { DateScalar } from '../common/scalars/date.scalar';
@@ -7,7 +6,6 @@ import { RecipesService } from './recipes.service';
 
 @Module({
   imports: [SurrealDbModule],
-  providers: [RecipesResolver, RecipesService, DateScalar, SurrealDbService],
-  exports: [SurrealDbService, SurrealDbService],
+  providers: [RecipesResolver, RecipesService, DateScalar],
 })
 export class RecipesModule { }
