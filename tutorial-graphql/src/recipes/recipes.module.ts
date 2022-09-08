7ed50aca23c9034ae7fe0061ad843f656a9677e7
Mário Monteiro <marioammonteiro@gmail.com>
2022-09-08 12:03:55 +0100

    push

--- tutorial-graphql/src/recipes/recipes.module.ts
+++ tutorial-graphql/src/recipes/recipes.module.ts
@@ -1,11 +1,24 @@
 import { SurrealDbModule, SurrealDbService } from '@koakh/nestjs-surrealdb';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DateScalar } from '../common/scalars/date.scalar';
 import { RecipesResolver } from './recipes.resolver';
 import { RecipesService } from './recipes.service';
 
 @Module({
-  imports: [SurrealDbModule],
+  imports: [
+    SurrealDbModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        url: configService.get('SURREALDB_URL'),
+        namespace: configService.get('SURREALDB_NAMESPACE'),
+        database: configService.get('SURREALDB_DATABASE'),
+        user: configService.get('SURREALDB_USER'),
+        pass: configService.get('SURREALDB_PASS'),
+      }),
+    }),
+  ],
   providers: [RecipesResolver, RecipesService, DateScalar],
 })
 export class RecipesModule { }
